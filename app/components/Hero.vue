c9d0a6a6de7f0f8104ef74217116b3f11c9d37de
ayarmkt <ayat.0906@gmail.com>
2025-10-02 19:42:48 +0200

    :feat: :heavy_plus_sign: added tailwind

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section>This is a hero</section>
+  <section class="h-screen flex flex-col justify-center items-center w-1/2">
+    <Typo tag="h1" variant="title">Aya Takamura</Typo>
+    <Typo tag="p" variant="text">
+      <Typo tag="p" variant="text" class="text-royal-blue font-black">
+        a Brand Marketer-turned-Front-End Developer
+      </Typo>
+      who cares about user experience. I am passionate about understanding
+      customers' needs and I adopt my user-centric mindset to create easy-to-use
+      applications.
+    </Typo>
+  </section>
 </template>
---
+++ app/components/Typo.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const variantClasses = {
+  title: 'text-5xl md:text-8xl font-black',
+  text: 'text-base md:text-xl text-dark-gray',
+};
+
+const { tag = 'p', variant = 'text' } = defineProps<{
+  // Tag name
+  tag?: string;
+  //   Variant
+  variant?: keyof typeof variantClasses;
+}>();
+</script>
+
+<template>
+  <component :is="tag" :class="variantClasses[variant]">
+    <slot />
+  </component>
+</template>
