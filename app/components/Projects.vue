4e61134447afe9351cd4bcd236dc12f6d8c54be1
ayarmkt <ayat.0906@gmail.com>
2025-10-03 20:10:11 +0200

    feat: :art: added projects section

--- app/components/Projects.vue
+++ app/components/Projects.vue
@@ -1,5 +1,69 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const projects = [
+  {
+    title: 'title 1',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expeditasapiente fugit doloribus vero eos ea accusamus iusto dolores nesciunt',
+    github: 'test.com',
+    demo: 'test.com',
+  },
+  {
+    title: 'title 2',
+    text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expeditasapiente fugit doloribus vero eos ea accusamus iusto dolores nesciunt',
+    github: 'test.com',
+    demo: 'test.com',
+  },
+];
+</script>
 
 <template>
-  <p>This is a project</p>
+  <section class="py-20 px-6">
+    <div class="max-w-4xl mx-auto">
+      <Typo tag="h2" variant="title-medium" class="pb-8 text-center">
+        Projects
+      </Typo>
+      <ul class="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6">
+        <li
+          v-for="(project, index) in projects"
+          :key="index"
+          class="shadow-md rounded-2xl overflow-hidden"
+        >
+          <div class="bg-gray-500 aspect-video"></div>
+          <div class="px-4 py-6 flex flex-col gap-y-4">
+            <Typo tag="h3" variant="title-small" class="text-center">
+              {{ project.title }}
+            </Typo>
+            <Typo tag="p" variant="text-small" class="text-center">
+              {{ project.text }}
+            </Typo>
+            <div class="flex gap-x-2">
+              <NuxtLink
+                :to="project.github"
+                class="w-full rounded-full border border-gray-600 py-1 px-2"
+              >
+                <Typo
+                  tag="span"
+                  variant="text-small"
+                  class="text-center flex justify-center"
+                >
+                  GitHub
+                </Typo>
+              </NuxtLink>
+              <NuxtLink
+                :to="project.demo"
+                class="w-full rounded-full bg-blue-700 py-1 px-2"
+              >
+                <Typo
+                  tag="span"
+                  variant="text-small"
+                  class="text-center text-white flex justify-center"
+                >
+                  Demo
+                </Typo>
+              </NuxtLink>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
