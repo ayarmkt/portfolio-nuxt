46df46eda31fbed86e4be2faf768b5b66b76a50a
ayarmkt <ayat.0906@gmail.com>
2025-10-03 21:31:36 +0200

    refactor: :art: created section container

--- app/components/Projects.vue
+++ app/components/Projects.vue
@@ -16,36 +16,34 @@ const projects = [
 </script>
 
 <template>
-  <section class="py-20 md:py-32 px-6">
-    <div class="max-w-4xl mx-auto">
-      <Typo tag="h2" variant="title-medium" class="pb-8 text-center">
-        Projects
-      </Typo>
-      <ul class="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6">
-        <li
-          v-for="(project, index) in projects"
-          :key="index"
-          class="shadow-md rounded-2xl overflow-hidden"
-        >
-          <div class="bg-gray-500 aspect-video"></div>
-          <div class="px-4 py-6 flex flex-col gap-y-4">
-            <Typo tag="h3" variant="title-small" class="text-center">
-              {{ project.title }}
-            </Typo>
-            <Typo tag="p" variant="text-small" class="text-center">
-              {{ project.text }}
-            </Typo>
-            <div class="flex gap-x-2">
-              <Button
-                label="GitHub"
-                :link="project.github"
-                variant="outline"
-              ></Button>
-              <Button label="Demo" :link="project.demo" variant="blue"></Button>
-            </div>
+  <SectionContainer>
+    <Typo tag="h2" variant="title-medium" class="pb-8 text-center">
+      Projects
+    </Typo>
+    <ul class="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-6">
+      <li
+        v-for="(project, index) in projects"
+        :key="index"
+        class="shadow-md rounded-2xl overflow-hidden"
+      >
+        <div class="bg-gray-500 aspect-video"></div>
+        <div class="px-4 py-6 flex flex-col gap-y-4">
+          <Typo tag="h3" variant="title-small" class="text-center">
+            {{ project.title }}
+          </Typo>
+          <Typo tag="p" variant="text-small" class="text-center">
+            {{ project.text }}
+          </Typo>
+          <div class="flex gap-x-2">
+            <Button
+              label="GitHub"
+              :link="project.github"
+              variant="outline"
+            ></Button>
+            <Button label="Demo" :link="project.demo" variant="blue"></Button>
           </div>
-        </li>
-      </ul>
-    </div>
-  </section>
+        </div>
+      </li>
+    </ul>
+  </SectionContainer>
 </template>
---
+++ app/components/SectionContainer.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="py-20 md:py-32 px-6">
+    <div class="max-w-4xl mx-auto">
+      <slot />
+    </div>
+  </section>
+</template>
