ce22d0b81e9bf0f36bfce1b59ddf929ec28e83a0
ayarmkt <ayat.0906@gmail.com>
2025-10-10 10:20:56 +0200

    feat: :sparkles: added naivgation menu

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -1,34 +1,38 @@
 <script setup lang="ts">
-const menuItems = [
+const menuItems: { label: string; link: string }[] = [
   {
     label: 'About me',
-    id: '#about',
+    link: '#about',
   },
   {
     label: 'Projects',
-    id: '#projects',
+    link: '#projects',
   },
   {
     label: 'Resume',
-    link: 'test.com',
+    link: 'https://test.com',
   },
 ];
 </script>
 
 <template>
   <!-- TODO: remove backdrop at the top of the page. Add backdrop on scroll -->
-  <nav
-    class="fixed top-0 w-full h-16 flex justify-center bg-white shadow-md backdrop-blur-md"
+  <UiNavigationMenu
+    class="fixed top-0 !items-stretch w-full !max-w-none h-16 bg-white shadow-md backdrop-blur-m"
   >
-    <ul class="flex gap-x-10 md:gap-x-16">
-      <li v-for="(menu, index) in menuItems" :key="index">
-        <NuxtLink
-          :to="menu.link || { hash: menu.id }"
-          class="h-full flex items-center"
+    <UiNavigationMenuList class="h-full gap-x-10 md:gap-x-16">
+      <UiNavigationMenuItem
+        v-for="(menu, index) in menuItems"
+        :key="index"
+        class="h-full my-auto"
+      >
+        <UiNavigationMenuLink
+          :href="menu.link"
+          class="h-full justify-center hover:!bg-transparent"
         >
           <Typo variant="text-small">{{ menu.label }}</Typo>
-        </NuxtLink>
-      </li>
-    </ul>
-  </nav>
+        </UiNavigationMenuLink>
+      </UiNavigationMenuItem>
+    </UiNavigationMenuList>
+  </UiNavigationMenu>
 </template>
